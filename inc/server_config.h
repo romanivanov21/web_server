2141e1ec0b259ca627130a87822ec651028dbc46
Ivanov Roman <romanivanov21@mail.ru>
2016-07-10 13:32:37 +0300

    Реализована возможность собирпть debug и release врсиии скрпитом make.sh

---
+++ inc/server_config.h
@@ -0,0 +1,10 @@
+#ifndef _CONFIG_SERVER_H_
+#define _CONFIG_SERVER_H_
+
+#include "master_config.h"
+
+class config_server : public master_config
+{
+    
+};
+#endif //_CONFIG_SERVER_H_
--- make.sh
+++ make.sh
@@ -10,7 +10,7 @@ DIR=$(cd $(dirname $0) && pwd);
 BUILD_DIR="$DIR/build";
 
 #if folder build ton found 
-if ! [[ -d $BUILD_DIR ]]; then
+if ! [[ -d "$BUILD_DIR/" ]]; then
 	echo "ERROR: directory $BUILD_DIR not found"
 	exit
 #if file CMakeLists.txt not found
@@ -18,8 +18,21 @@ else if ! [[ -f "$BUILD_DIR/CMakeLists.txt" ]]; then
 	echo "ERROR: file CMakeLists.txt from '$BUILD_DIR' not found"
 	exit
 else
-	#run command cmake
-	cmake "$BUILD_DIR/CMakeLists.txt"
+    case "$1" in
+        "-d"    )
+	    #run command cmake for debug
+	    cmake "$BUILD_DIR/CMakeLists.txt" -DCMAKE_BUILD_TYPE=Debug
+	    ;;
+	    "-r"    )
+	    #run command cmake for release
+	    cmake "$BUILD_DIR/CMakeLists.txt" -DCMAKE_BUILD_TYPE=Release
+	    ;;
+	    *)
+	    echo "Options:"
+        echo "  -d       Build debug version"
+	    echo "  -r       Build release version"
+	    exit 25
+	esac
 	#if Makefile not found 
 	if ! [[ -f "$BUILD_DIR/Makefile" ]]; then
 		echo "ERROR: make file from $BUILD_DIR not found"
@@ -32,23 +45,3 @@ else
 	fi
 fi
 fi
-
-#if output file not found
-if ! [[ -f "$BUILD_DIR/$PROJECT" ]]; then
-	echo "ERROR: execute file '$PROJECT' not found"
-	exit
-#if foldef 'bin' and output file found 
-else if [[ -d "$BUILD_DIR/bin" ]] && ! [[ -f "$BUILD_DIR/bin/$PROJECT" ]]; then
-	mv "$BUILD_DIR/$PROJECT" "$BUILD_DIR/bin" && echo "-- Move new executable file '$PROJECT' from $BUILD_DIR/ to $BUILD_DIR/bin/"
-#if folder 'bin' found and output file not found
-else if [[ -d "$BUILD_DIR/bin" ]] && [[ -f "$BUILD_DIR/bin/$PROJECT" ]]; then
-	rm -R "$BUILD_DIR/bin/$PROJECT"
-	mv "$BUILD_DIR/$PROJECT" "$BUILD_DIR/bin" && echo "-- Move new executable file '$PROJECT' from $BUILD_DIR/ to $BUILD_DIR/bin/"
-else
-#if folder 'bin' nit found
-	mkdir "$BUILD_DIR/bin" && echo "-- Create folder 'bin' for the executable files"
-	mv "$BUILD_DIR/$PROJECT" "$BUILD_DIR/bin" && echo "-- Move executable file '$PROJECT' from $BUILD_DIR/ to $BUILD_DIR/bin/"
-fi
-fi
-fi
-
