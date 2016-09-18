b7557b6daa0d49da9c80c10c8d80a646b8b13e9e
Aleksey Pashinov <pashinov@outlook.com>
2016-09-18 12:06:23 +0300

    Добавил проверку наличия файла запуска при запуске программы. Добавил завершение процесса демона при неудачной записи его pid в файл. ДОбавил предыдущие изменения, касающиеся реализации логирования, так как после слияния я их не наблюдал в главной ветке

--- src/logs/access_log.cpp
+++ src/logs/access_log.cpp
@@ -1,27 +1,15 @@
-#include <common/user_msg/msg_type.h>
-#include "serevr_log_exception.h"
 #include "access_log.h"
 
-access_log* access_log::log_ = nullptr;
-destroyer_singleton<access_log> access_log::destroyer_;
+access_log* access_log::log = nullptr;
+destroyer_singleton<access_log> access_log::destroyer;
 
 access_log* access_log::get_instance() noexcept
 {
-    if ( !log_ )
+    if (!log)
     {
-        log_ = new access_log();
-        destroyer_.init_obj( log_ );
+        log = new access_log;
+        destroyer.init_obj(log);
     }
-    return log_;
-}
 
-void access_log::init_log_file( const std::string& file_name )
-{
-    assert( !file_name.empty() );
-
-    filename_ = file_name;
-    if ( !save_log( "logfile initialization complete" ) )
-    {
-        throw server_log_exception( msg_type::msg_init_access_log_err );
-    }
+    return log;
 }
