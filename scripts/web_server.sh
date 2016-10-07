c2d2a7a88868d81ec1844d24447d2e4aa79cf537
Aleksey Pashinov <pashinov@outlook.com>
2016-10-07 16:57:32 +0300

    Добавил запуск в режиме отладки

--- scripts/web_server.sh
+++ scripts/web_server.sh
@@ -6,6 +6,7 @@ PATH_DAEMON=/usr/local/bin/
 DAEMON=$NAME
 PATH_PIDFILE=/var/run/
 PIDFILE=$NAME.pid
+PATH_DAEMON_DEBUG=../build/bin/
 
 case "$1" in 
 
@@ -14,7 +15,7 @@ case "$1" in
         if [ ! -f $DAEMON ]; then
             echo "run file $DAEMON not found"
         else
-            $DAEMON
+            ./$DAEMON
         fi
         ;;
 
@@ -36,14 +37,52 @@ case "$1" in
     stop) 
         cd $PATH_PIDFILE
         kill $(cat $PIDFILE)
-        printf "%-50s\n" "[ ok ] PID=$(cat $PIDFILE) kill"
         rm -f $PIDFILE
-        printf "%-50s\n" "Stopping $DAEMON..."
         ;;
 
     restart)
         sh $0 stop
         sh $0 start
+        ;;
+
+    -d)
+        case "$2" in 
+
+            start) 
+                cd $PATH_DAEMON_DEBUG
+                if [ ! -f $DAEMON ]; then
+                    echo "run file $DAEMON not found"
+                else
+                    ./$DAEMON
+                fi
+                ;;
+
+            status)
+                printf "%-50s" "Checking $DAEMON..."
+                cd $PATH_PIDFILE
+                if [ -f $PIDFILE ]; then
+                    PID=$(cat $PIDFILE)
+                    if [ -z "`ps axf | grep ${PID} | grep -v grep`" ]; then
+                        printf "%s\n" "Process dead but pidfile exists"
+                    else
+                        echo "Running"
+                    fi
+                else
+                    printf "%s\n" "Service not running"
+                fi
+                ;;
+
+            stop) 
+                cd $PATH_PIDFILE
+                kill $(cat $PIDFILE)
+                rm -f $PIDFILE
+                ;;
+
+            restart)
+                sh $0 -d stop
+                sh $0 -d start
+                ;;
+        esac
         ;;
 
     *)
--- src/logs/access_log.cpp
+++ src/logs/access_log.cpp
@@ -1,6 +1,8 @@
 #include <common/user_msg/msg_type.h>
 #include "serevr_log_exception.h"
 #include "access_log.h"
+#include "types.h"
+#include "sys/stat.h"
 
 access_log* access_log::log_ = nullptr;
 destroyer_singleton<access_log> access_log::destroyer_;
@@ -18,6 +20,20 @@ access_log* access_log::get_instance() noexcept
 void access_log::init_log_file( const std::string& file_name )
 {
     assert( !file_name.empty() );
+
+#ifdef _DEBUG
+    int ret = mkdir("/var/log/echo-server", 0644);
+    if (ret == default_error_code)
+    {
+        save_log( "Debug: can not create directory /var/log/echo-server" );
+        save_log( "Debug: logfile initialization failed" );
+        throw server_log_exception( msg_type::msg_init_error_log_err );
+    }
+    else if (ret == EEXIST)
+    {
+        save_log( "Debug: Directory already exists" );
+    }
+#endif //_DEBUG
 
     filename_ = file_name;
     if ( !save_log( "logfile initialization complete" ) )
