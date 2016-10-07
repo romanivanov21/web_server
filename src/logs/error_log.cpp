4e122f1cb67932c9a58c95cc87e7938197e05240
Aleksey Pashinov <pashinov@outlook.com>
2016-10-07 17:04:31 +0300

    Добавил запуск в режиме отладки

--- src/logs/error_log.cpp
+++ src/logs/error_log.cpp
@@ -1,6 +1,9 @@
 #include <common/user_msg/msg_type.h>
 #include "error_log.h"
 #include "serevr_log_exception.h"
+#include "types.h"
+
+#include "sys/stat.h"
 
 error_log* error_log::log = nullptr;
 destroyer_singleton<error_log> error_log::destroyer;
@@ -18,6 +21,21 @@ error_log* error_log::get_instance() noexcept
 void error_log::init_log_file( const std::string& file_name )
 {
     assert( !file_name.empty() );
+
+#ifdef _DEBUG
+    int ret = mkdir("/var/log/echo-server", 0644);
+    if (ret == default_error_code)
+    {
+        save_log( "Debug: can not create directory /var/log/echo-server" );
+        save_log( "Debug: logfile initialization failed" );
+        throw server_log_exception( msg_type::msg_init_error_log_err );
+    }
+    else if (ret == EEXIST)
+    {
+        save_log( "Debug: Directory already exists" );
+    }
+#endif //_DEBUG
+
     filename_ = file_name;
 
     if (!save_log("logfile initialization complete"))
