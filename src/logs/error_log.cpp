7cc75bbaafae59a9e09517243b574f09ae3e9d16
Aleksey Pashinov <pashinov@outlook.com>
2016-10-12 18:06:23 +0300

    Удален режим отладки в файлах логирования

--- src/logs/error_log.cpp
+++ src/logs/error_log.cpp
@@ -22,20 +22,6 @@ void error_log::init_log_file( const std::string& file_name )
 {
     assert( !file_name.empty() );
 
-#ifdef _DEBUG
-    int ret = mkdir("/var/log/echo-server", 0644);
-    if (ret == default_error_code)
-    {
-        save_log( "Debug: can not create directory /var/log/echo-server" );
-        save_log( "Debug: logfile initialization failed" );
-        throw server_log_exception( msg_type::msg_init_error_log_err );
-    }
-    else if (ret == EEXIST)
-    {
-        save_log( "Debug: Directory already exists" );
-    }
-#endif //_DEBUG
-
     filename_ = file_name;
 
     if (!save_log("logfile initialization complete"))
