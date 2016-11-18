ff06443b5a0d12b8d6736b2409b23022c7fea1db
Ivanov Roman <romanivanov21@mail.ru>
2016-11-19 02:23:31 +0300

    Реализован разбор старшего числа версии HTTP проткола

--- http_lib/inc/lib/http_request_parser.h
+++ http_lib/inc/lib/http_request_parser.h
@@ -9,7 +9,7 @@
 class http_request_parser
 {
 public:
-    enum class parser_state
+    enum class parser_state : uint64_t
     {
         request_method_begin = 0,
         request_method,
@@ -17,8 +17,13 @@ public:
         request_uri,
         request_http_version_h,
         request_http_version_ht,
+        request_http_version_htt,
         request_http_version_http,
-        request_http_version_separator
+        request_http_version_separator,
+        request_http_version_major,
+        request_http_version_point,
+        request_http_version_minor_state,
+        request_http_version_minor
     };
 
     enum class request_parse_result
@@ -75,8 +80,24 @@ private:
      */
     http_request_parser::request_parse_result parse_request_uri_begin( const char& c, http_request& request ) noexcept;
 
+    /**
+     *  @brief парсинг URI
+     */
     http_request_parser::request_parse_result parse_request_uri( const char& c, http_request& request ) noexcept;
 
+    http_request_parser::request_parse_result parse_request_http_version_h( const char& c ) noexcept;
+
+    http_request_parser::request_parse_result parse_request_http_version_ht( const char& c ) noexcept;
+
+    http_request_parser::request_parse_result parse_request_http_version_htt( const char& c ) noexcept;
+
+    http_request_parser::request_parse_result parse_request_http_version_http( const char& c ) noexcept;
+
+    http_request_parser::request_parse_result parse_request_http_version_separator( const char& c ) noexcept;
+
+    http_request_parser::request_parse_result parse_request_http_version_major( const char& c, http_request& request ) noexcept;
+
+    http_request_parser::request_parse_result parse_request_http_version_point( const char& c, http_request& request) noexcept;
     /**
      * @brief метод для проверки, является ли символ из HTTP запроса допустимым ASCII кодом
      *
@@ -98,11 +119,20 @@ private:
     /**
      * @brief метод для проверки, является ли символ из HTTP запроса специальным символом
      *
-     * @param c символ для проверки
+     * @param символ для проверки
      *
-     * @return true если смвол дявляется допустимым, false - иначе
+     * @return true если символ дявляется допустимым, false - иначе
      */
     bool is_special_char( const char& c ) const noexcept;
+
+    /**
+     * @break проверка, является ли симовл числом
+     *
+     * @param символ для проверки
+     *
+     * @return true если символ явялется числом, false - иначе
+     */
+    bool is_digit( const char& c ) const noexcept;
 
 private:
     parser_state state_;
--- http_lib/src/lisb/http_request_parser.cpp
+++ http_lib/src/lisb/http_request_parser.cpp
@@ -34,9 +34,40 @@ http_request_parser::request_parse_result http_request_parser::parse( const std:
                 result = parse_request_uri( *it, request );
                 break;
             }
-            default:
+            case http_request_parser::parser_state::request_http_version_h:
             {
-                result = http_request_parser::request_parse_result::INCOMPLETE;
+                result = parse_request_http_version_h( *it );
+                break;
+            }
+            case http_request_parser::parser_state::request_http_version_ht:
+            {
+                result = parse_request_http_version_ht( *it );
+                break;
+            }
+            case http_request_parser::parser_state::request_http_version_htt:
+            {
+                result = parse_request_http_version_htt( *it );
+                break;
+            }
+            case http_request_parser::parser_state::request_http_version_http:
+            {
+                result = parse_request_http_version_http( *it );
+                break;
+            }
+            case http_request_parser::parser_state::request_http_version_separator:
+            {
+                result = parse_request_http_version_separator( *it );
+                break;
+            }
+            case http_request_parser::parser_state::request_http_version_major:
+            {
+                result = parse_request_http_version_major( *it, request );
+                break;
+            }
+            case http_request_parser::parser_state::request_http_version_point:
+            {
+                result = parse_request_http_version_point( *it, request );
+                break;
             }
         }
 
@@ -115,6 +146,87 @@ http_request_parser::request_parse_result http_request_parser::parse_request_uri
     return result;
 }
 
+http_request_parser::request_parse_result http_request_parser::parse_request_http_version_h( const char &c ) noexcept
+{
+    auto result = http_request_parser::request_parse_result::ERROR;
+    if( c == 'H' )
+    {
+        state_ = http_request_parser::parser_state::request_http_version_ht;
+        result = http_request_parser::request_parse_result::COMPLETE;
+    }
+    return result;
+}
+
+http_request_parser::request_parse_result http_request_parser::parse_request_http_version_ht( const char &c ) noexcept
+{
+    auto result = http_request_parser::request_parse_result::ERROR;
+    if( c == 'T' )
+    {
+        state_ = http_request_parser::parser_state::request_http_version_htt;
+        result = http_request_parser::request_parse_result::COMPLETE;
+    }
+    return result;
+}
+
+http_request_parser::request_parse_result http_request_parser::parse_request_http_version_htt( const char &c ) noexcept
+{
+    auto result = http_request_parser::request_parse_result::ERROR;
+    if( c == 'T' )
+    {
+        state_ = http_request_parser::parser_state::request_http_version_http;
+        result = http_request_parser::request_parse_result::COMPLETE;
+    }
+    return result;
+}
+
+http_request_parser::request_parse_result http_request_parser::parse_request_http_version_http( const char &c ) noexcept
+{
+    auto result = http_request_parser::request_parse_result::ERROR;
+    if( c == 'P' )
+    {
+        state_ = http_request_parser::parser_state::request_http_version_separator;
+        result = http_request_parser::request_parse_result::COMPLETE;
+    }
+    return result;
+}
+
+http_request_parser::request_parse_result http_request_parser::parse_request_http_version_separator( const char &c ) noexcept
+{
+    auto result = http_request_parser::request_parse_result::ERROR;
+    if( c == '/' )
+    {
+        state_ = http_request_parser::parser_state::request_http_version_major;
+        result = http_request_parser::request_parse_result::COMPLETE;
+    }
+    return result;
+}
+
+http_request_parser::request_parse_result http_request_parser::parse_request_http_version_major( const char &c, http_request& request ) noexcept
+{
+    auto result = http_request_parser::request_parse_result::ERROR;
+    if( is_digit( c ) )
+    {
+        request.version_major_ = std::atoi( &c );
+        state_ = http_request_parser::parser_state::request_http_version_point;
+    }
+    return result;
+}
+
+http_request_parser::request_parse_result http_request_parser::parse_request_http_version_point( const char &c, http_request& request ) noexcept
+{
+    auto result = http_request_parser::request_parse_result::ERROR;
+    if( c == '.' )
+    {
+        state_ = http_request_parser::parser_state::request_http_version_minor;
+        result = http_request_parser::request_parse_result::COMPLETE;
+    }
+    else if( is_digit( c ) )
+    {
+        request.version_major_ = request.version_major_ * 10 + c;
+    }
+    return result;
+}
+
 bool http_request_parser::is_valid_ASCII_char( const char& c ) const noexcept
 {
     return ( ( c >= 0 ) && ( c <= 127 ) );
@@ -137,4 +249,9 @@ bool http_request_parser::is_special_char( const char &c ) const noexcept
         default:
             return false;
     }
+}
+
+bool http_request_parser::is_digit( const char &c ) const noexcept
+{
+    return ( ( c >= '0' ) && ( c <= '9' ) );
 }
