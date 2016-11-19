8f35be3e9e82797752ba7f2e974d27d821e94926
Ivanov Roman <romanivanov21@mail.ru>
2016-11-20 02:29:19 +0300

    Исправлена ошибка разбора версии HTTP проткола и реализовано разбор HTTP заголовков

--- http_lib/inc/lib/http_request_parser.h
+++ http_lib/inc/lib/http_request_parser.h
@@ -1,3 +1,9 @@
+/**
+ *  Файл: http_request_parser.h
+ *
+ *  Описание: Парсер запроса HTTP
+ */
+
 #ifndef _HTTP_REQUEST_PARSER_H_
 #define _HTTP_REQUEST_PARSER_H_
 
@@ -22,8 +28,13 @@ public:
         request_http_version_separator,
         request_http_version_major,
         request_http_version_point,
-        request_http_version_minor_state,
-        request_http_version_minor
+        request_http_version_minor_begin,
+        request_http_version_minor_end,
+        request_http_version_new_line,
+        request_header_begin,
+        request_header_name,
+        request_before_name_space,
+        request_header_value
     };
 
     enum class request_parse_result
@@ -38,7 +49,7 @@ public:
 
     ~http_request_parser() = default;
 
-    request_parse_result parse( const std::string& src_request, http_request& request );
+    request_parse_result parse( const std::string& src_request, http_request& request ) noexcept;
 
     http_request_parser( const http_request_parser& copy ) = delete;
 
@@ -98,6 +109,21 @@ private:
     http_request_parser::request_parse_result parse_request_http_version_major( const char& c, http_request& request ) noexcept;
 
     http_request_parser::request_parse_result parse_request_http_version_point( const char& c, http_request& request) noexcept;
+
+    http_request_parser::request_parse_result parse_request_http_version_minor_begin( const char& c, http_request& request ) noexcept;
+
+    http_request_parser::request_parse_result parse_request_http_version_minor_end( const char& c, http_request& request ) noexcept;
+
+    http_request_parser::request_parse_result parse_request_http_version_new_line( const char& c ) noexcept;
+
+    http_request_parser::request_parse_result parse_request_header_begin( const char& c, http_request& request ) noexcept;
+
+    http_request_parser::request_parse_result parse_request_header_name( const char& c, http_request& request ) noexcept;
+
+    http_request_parser::request_parse_result parse_request_before_name_space( const char& c ) noexcept;
+
+    http_request_parser::request_parse_result parse_request_header_value( const char& c, http_request& request )noexcept;
+
     /**
      * @brief метод для проверки, является ли символ из HTTP запроса допустимым ASCII кодом
      *
@@ -133,9 +159,18 @@ private:
      * @return true если символ явялется числом, false - иначе
      */
     bool is_digit( const char& c ) const noexcept;
+private:
+    struct header
+    {
+        header() : name_( "" ), value_( "" ) { }
+
+        std::string name_;
+        std::string value_;
+    };
 
 private:
     parser_state state_;
+    header current_header_;
 };
 
 #endif //_HTTP_REQIEST_H_
--- http_lib/src/lisb/http_request_parser.cpp
+++ http_lib/src/lisb/http_request_parser.cpp
@@ -2,15 +2,15 @@
 
 #include <cassert>
 
-http_request_parser::http_request_parser() : state_( parser_state::request_method_begin ) { }
+http_request_parser::http_request_parser() : state_( parser_state::request_method_begin ), current_header_() { }
 
-http_request_parser::request_parse_result http_request_parser::parse( const std::string &src_request, http_request &request )
+http_request_parser::request_parse_result http_request_parser::parse( const std::string &src_request, http_request &request ) noexcept
 {
     assert( !src_request.empty() );
 
     auto result = http_request_parser::request_parse_result::COMPLETE;
-    auto it = src_request.begin();
-    while( it != src_request.end() )
+    auto it = src_request.cbegin();
+    while( it != src_request.cend() )
     {
         switch( state_ )
         {
@@ -67,6 +67,36 @@ http_request_parser::request_parse_result http_request_parser::parse( const std:
             case http_request_parser::parser_state::request_http_version_point:
             {
                 result = parse_request_http_version_point( *it, request );
+                break;
+            }
+            case http_request_parser::parser_state::request_http_version_minor_begin:
+            {
+                result = parse_request_http_version_minor_begin( *it, request );
+                break;
+            }
+            case http_request_parser::parser_state::request_http_version_minor_end:
+            {
+                result = parse_request_http_version_minor_end( *it, request );
+                break;
+            }
+            case http_request_parser::parser_state::request_http_version_new_line:
+            {
+                result = parse_request_http_version_new_line( *it );
+                break;
+            }
+            case http_request_parser::parser_state::request_header_begin:
+            {
+                result = parse_request_header_begin( *it, request );
+                break;
+            }
+            case http_request_parser::parser_state::request_before_name_space:
+            {
+                result = parse_request_before_name_space( *it );
+                break;
+            }
+            case http_request_parser::parser_state::request_header_value:
+            {
+                result = parse_request_header_value( *it, request );
                 break;
             }
         }
@@ -206,8 +236,9 @@ http_request_parser::request_parse_result http_request_parser::parse_request_htt
     auto result = http_request_parser::request_parse_result::ERROR;
     if( is_digit( c ) )
     {
-        request.version_major_ = std::atoi( &c );
+        request.version_major_ = c - '0';
         state_ = http_request_parser::parser_state::request_http_version_point;
+        result = http_request_parser::request_parse_result::COMPLETE;
     }
     return result;
 }
@@ -217,12 +248,126 @@ http_request_parser::request_parse_result http_request_parser::parse_request_htt
     auto result = http_request_parser::request_parse_result::ERROR;
     if( c == '.' )
     {
-        state_ = http_request_parser::parser_state::request_http_version_minor;
+        state_ = http_request_parser::parser_state::request_http_version_minor_begin;
         result = http_request_parser::request_parse_result::COMPLETE;
     }
     else if( is_digit( c ) )
     {
-        request.version_major_ = request.version_major_ * 10 + c;
+        request.version_major_ = request.version_major_ * 10 + c - '0';
+    }
+    return result;
+}
+
+http_request_parser::request_parse_result http_request_parser::parse_request_http_version_minor_begin( const char &c, http_request& request ) noexcept
+{
+    auto result = http_request_parser::request_parse_result::ERROR;
+
+    if( is_digit( c ) )
+    {
+        request.version_minor_ = c - '0';
+        state_ = parser_state::request_http_version_minor_end;
+        result = http_request_parser::request_parse_result::COMPLETE;
+    }
+    return result;
+}
+
+http_request_parser::request_parse_result http_request_parser::parse_request_http_version_minor_end( const char &c, http_request &request ) noexcept
+{
+    auto result = http_request_parser::request_parse_result::COMPLETE;
+    if( is_digit( c ) )
+    {
+        request.version_minor_ = request.version_minor_ * 10 + c - '0';
+    }
+    else if( c == '\r' )
+    {
+        state_ = parser_state::request_http_version_new_line;
+    }
+    else
+    {
+        result = http_request_parser::request_parse_result::ERROR;
+    }
+    return result;
+}
+
+http_request_parser::request_parse_result http_request_parser::parse_request_http_version_new_line( const char &c ) noexcept
+{
+    auto result = http_request_parser::request_parse_result::ERROR;
+    if( c == '\n' )
+    {
+        state_ = parser_state::request_header_begin;
+        result = http_request_parser::request_parse_result::COMPLETE;
+    }
+    return result;
+}
+
+http_request_parser::request_parse_result http_request_parser::parse_request_header_begin( const char &c, http_request& request ) noexcept
+{
+    auto result = http_request_parser::request_parse_result::COMPLETE;
+    if( !is_valid_ASCII_char( c ) || !is_valid_HTTP_char( c ) || is_special_char( c ) )
+    {
+        result = http_request_parser::request_parse_result::ERROR;
+    }
+    else if( c == '\r' )
+    {
+        //TODO
+    }
+    else
+    {
+        current_header_.name_.push_back( c );
+        state_ = parser_state::request_header_name;
+    }
+    return result;
+}
+
+http_request_parser::request_parse_result http_request_parser::parse_request_header_name( const char &c, http_request &request ) noexcept
+{
+    auto result = http_request_parser::request_parse_result::COMPLETE;
+    if( c == ':' )
+    {
+        state_ = parser_state::request_before_name_space;
+    }
+    else if ( !is_valid_ASCII_char( c ) || !is_valid_HTTP_char( c ) || is_special_char( c ) )
+    {
+        result = http_request_parser::request_parse_result::ERROR;
+    }
+    else
+    {
+        current_header_.name_.push_back( c );
+    }
+    return result;
+}
+
+http_request_parser::request_parse_result http_request_parser::parse_request_before_name_space( const char &c ) noexcept
+{
+    auto result = http_request_parser::request_parse_result::ERROR;
+    if( c == ' ' )
+    {
+        state_ = http_request_parser::parser_state::request_header_value;
+        result = http_request_parser::request_parse_result::COMPLETE;
+    }
+    return result;
+}
+
+http_request_parser::request_parse_result http_request_parser::parse_request_header_value( const char &c, http_request &request ) noexcept
+{
+    auto result = http_request_parser::request_parse_result::ERROR;
+    if( c == '\r' )
+    {
+        if( ( !current_header_.name_.empty() ) && ( !current_header_.value_.empty() ) )
+        {
+            std::pair<std::string, std::string> header( current_header_.name_, current_header_.value_ );
+            request.headers_.insert( header );
+            result = http_request_parser::request_parse_result::COMPLETE;
+        }
+    }
+    else if( !is_valid_HTTP_char( c ) )
+    {
+        result = http_request_parser::request_parse_result::ERROR;
+    }
+    else
+    {
+        current_header_.value_.push_back( c );
+        result = http_request_parser::request_parse_result::COMPLETE;
     }
     return result;
 }
